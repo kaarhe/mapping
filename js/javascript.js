a1ca3baf14d75867d88de59e85fc8d0afd830824
kaarhe <104354848+kaarhe@users.noreply.github.com>
2022-04-25 12:49:24 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,10 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: 'OpenStreetMap contributors',
 })
 osm.addTo(map)
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data)
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
